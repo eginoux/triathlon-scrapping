399e688746db7c0ee0f35694eeb0f150b76227d0
eginoux <ginoux.etienne@gmail.com>
2023-09-25 12:08:28 +0200

    Code works for one page, trying to save data into a DF

--- scripts/scrapping.py
+++ scripts/scrapping.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 
 # Driver installation
 web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -16,19 +17,18 @@ url = "https://www.acn-timing.com/?lng=FR#/events/2141423449174261/ctx/20230725_
 web_driver.get(url)
 
 # Wait until presence of element
-WebDriverWait(web_driver, 20).until(EC.presence_of_element_located((By.XPATH, "//tbody[@class='table-date']")))
+WebDriverWait(web_driver, 20).until(EC.presence_of_element_located((By.XPATH, "//tbody[@class='table-data']")))
 
 # Set table and rows to go through
-table = web_driver.find_element(By.XPATH, "//tbody[@class='table-date']")
+table = web_driver.find_element(By.XPATH, "//tbody[@class='table-data']")
 rows = table.find_elements(By.XPATH, "//tr[@class='last-record-line']")
 
-# Get elements from each row
+# Get elements
 for i, row in enumerate(rows):
     out = []
     columns = row.find_elements(By.TAG_NAME, "td")
     for column in columns:
         out.append(column.text)
-    print(out)
 
 # Close driver
 web_driver.quit()
